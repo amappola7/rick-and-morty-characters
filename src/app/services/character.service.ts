10ece0e7f09eeec687b72e6a9c22b321736103ae
Amappola <anam.porrasp@gmail.com>
2023-10-19 17:37:47 -0500

    Creating character HTTP requests

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
+  private _url: string = 'https://rickandmortyapi.com/api/character';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getCharacters(): Observable<{}> {
+    return this.http.get(this._url);
+  }
+
+  getCharactersPaginated(page: number): Observable<{}> {
+    return this.http.get(`${this._url}?page=${page}`);
+  }
+
+  getCharacterById(id: number): Observable<{}> {
+    return this.http.get(`${this._url}/${id}`);
+  }
 }
