e9d5a3ade419cfcebd78c79624538fa149694c53
Amappola <anam.porrasp@gmail.com>
2023-10-20 08:59:19 -0500

    Creating paginator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CharacterService } from './services/character.service';
 import { ICharacter } from './interfaces/icharacter';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,25 @@ import { ICharacter } from './interfaces/icharacter';
 })
 export class AppComponent {
   charactersList!: ICharacter[];
+  paginator: FormControl  = new FormControl(1);
 
   constructor(
     private characterService: CharacterService
   ) {}
 
   ngOnInit() {
-    this.characterService.getCharacters(1)
+    this.getCharacters(1);
+  }
+
+  getCharacters(page: number): void {
+    this.characterService.getCharacters(page)
     .subscribe((list) => {
       const {info, results} = list;
       this.charactersList = results;
     });
+  }
+
+  onPaginatorChange() {
+    this.getCharacters(this.paginator.value);
   }
 }
