359a2d73bd09a3a9901ad13bcc0982f1344d99ae
Amappola <anam.porrasp@gmail.com>
2023-10-19 19:39:33 -0500

    Displaying the characters in the app component and setting up its styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CharacterService } from './services/character.service';
+import { ICharacter } from './interfaces/icharacter';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rick-and-morty';
+  charactersList!: ICharacter[];
+
+  constructor(
+    private characterService: CharacterService
+  ) {}
+
+  ngOnInit() {
+    this.characterService.getCharacters(1)
+    .subscribe((list) => {
+      const {info, results} = list;
+      this.charactersList = results;
+    });
+  }
 }
