07cf0bff223ea63df122a5ce4a5bc3c40190fb26
Amappola <anam.porrasp@gmail.com>
2023-10-27 10:08:46 -0500

    Using the take operator to unsubscribe from observables

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { speciesList } from '../data/species';
 
 @Injectable({
@@ -14,15 +14,21 @@ export class CharacterService {
   ) { }
 
   getCharacters(page: number): Observable<any> {
-    return this.http.get(`${this._url}?page=${page}`)
+    return this.http.get(`${this._url}?page=${page}`).pipe(
+      take(1)
+    );
   }
 
   getCharacterById(id: number): Observable<any> {
-    return this.http.get(`${this._url}/${id}`);
+    return this.http.get(`${this._url}/${id}`).pipe(
+      take(1)
+    );
   }
 
   getFilteredCharacters(page: number, filterType: string, filter: string): Observable<any> {
-    return this.http.get(`${this._url}?page=${page}&${filterType}=${filter}`)
+    return this.http.get(`${this._url}?page=${page}&${filterType}=${filter}`).pipe(
+      take(1)
+    );
   }
 
   getSpecies(): string[] {
